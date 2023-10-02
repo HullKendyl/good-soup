bed4a76cfd4c0c9f41bf485ea70234ee0e856019
Kendyl Hull <hullkendyl@gmail.com>
2023-10-01 23:14:37 -0400

    Created UpcomingWeather Component - First iteration

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import CurrentWeather from "./src/components/CurrentWeather";
+import UpcomingWeather from "./src/components/UpcomingWeather";
+import CityDetails from "./src/components/CityDetails";
 
 const App = () => {
   return (
-    <CurrentWeather />
+    //<CurrentWeather />
+    <UpcomingWeather />
   )
 }
 
